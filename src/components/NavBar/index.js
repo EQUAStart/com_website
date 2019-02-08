bdf7c616317b0e6dd752f5097cdd291cdb43d530
chuckwilliams37 <chuck@dash.org>
2019-02-08 11:41:21 -1000

    :pencil2::lipstick: CSS treatment & text fix to header menu for 'Equanauts' section

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -39,7 +39,7 @@ const NavBar = ({toggleNavbar, isActive}) => (
         <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id='navMenu'>
 
           <div className="navbar-end">
-            <a href='#equateam' className='navbar-item'>The Equa Team</a>
+            <a href='#equateam' className='navbar-item'>The Equanauts</a>
             <a href='#equacareers' className='navbar-item'>Careers</a>
             <a className='navbar-item' href='https://auth.merrillcorp.com/as/authorization.oauth2?client_id=Apigee&response_type=code&redirect_uri=https://datasiteone.merrillcorp.com/global/callback&state=&scope=openid' target='_blank'>
               Investor's Portal
