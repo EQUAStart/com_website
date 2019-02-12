b977cd052214c09116663945f97b1e4906ba4124
chuckwilliams37 <chuck@dash.org>
2019-02-12 06:01:09 -0600

    :bento::zap::sparkles: Adds Terms of use page and footer navigation.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import config from '../../../data/config'
 import {Link} from 'gatsby'
 
+
 const Footer = () => {
   return (
     <footer className='footer'>
@@ -9,6 +10,9 @@ const Footer = () => {
         <p>
           {config.copyright}
         </p>
+        <div className={`navbar-menu`} id='footerMenu'>
+          <Link to='/terms-of-use' className='navbar-item'>Terms of use</Link>
+        </div>
       </div>
     </footer>
   )
